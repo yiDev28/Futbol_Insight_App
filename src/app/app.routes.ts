71c7e81abfee7fc2ac80f7578b32973ddda910c0
Yiduard Andres Bolivar <yidev28@gmail.com>
2024-08-22 22:51:37 -0500

    Carga inicial

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import {  Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'signIn', component: SignInComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '**', redirectTo: 'login', pathMatch: 'full' },
+];
+
+
